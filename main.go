5dab3ca238f8d150d6c7819897039b6672e7690a
Pradip Caulagi <caulagi@gmail.com>
2021-12-05 19:14:11 +0100

    Test entire solution (#5)
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- main.go
+++ main.go
@@ -1,40 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"math"
-
 	"github.com/caulagi/x-knight-tour/board"
 )
 
 func main() {
 	startingX, startingY, boardSize := userInput()
-	visitCount := 1
 	b := board.NewBoard(boardSize, startingX, startingY)
-
-	for {
-		candidates := b.CurrentCandidates()
-		least := math.MaxInt8
-		leastTile := board.Tile{}
-		for _, candidate := range candidates {
-			nextMoves := b.Candidates(candidate.X, candidate.Y)
-			if len(nextMoves) < least {
-				least = len(nextMoves)
-				leastTile = candidate
-			}
-		}
-		visitCount += 1
-		b.Tiles[leastTile.X][leastTile.Y].Visit = visitCount
-		b.MoveToTile(leastTile)
-
-		if b.PositionX == startingX && b.PositionY == startingY {
-			fmt.Println("Failed to find a path; try again?")
-			break
-		}
-
-		if visitCount == boardSize*boardSize {
-			break
-		}
-	}
+	b.Solve(startingX, startingY, boardSize)
 	b.Print()
 }
